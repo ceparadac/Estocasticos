84f361b78704e9e5e82c14d8562ed612192c76c6
davidsneos <davidcamiloneo@gmail.com>
2015-10-31 00:14:04 -0500

    -Asignadas las direcciones ip para los 4 nodos fisicos de acuerdo a la tipologia y creadas las tablas de enrutamiento

--- tallerns3.cc
+++ tallerns3.cc
@@ -37,13 +37,40 @@ main (int argc, char *argv[])
         pointToPointHelper.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
         pointToPointHelper.SetChannelAttribute ("Delay", StringValue ("1ms"));
 
-        //Assing the point to point links to the respective nodes
+        //Assing the point to point links to the respective nodes (OSI Layer 1)
 
         NetDeviceContainer devices;
         devices.Add (pointToPointHelper.Install (cabledNodes.Get (0), cabledNodes.Get (2)));
         devices.Add (pointToPointHelper.Install (cabledNodes.Get (2), cabledNodes.Get (3)));
         devices.Add (pointToPointHelper.Install (cabledNodes.Get (3), cabledNodes.Get (1)));
         devices.Add (pointToPointHelper.Install (cabledNodes.Get (1), cabledNodes.Get (0)));
+
+
+        //Creating the ip stack for the connection (OSI Layer 3)
+
+        InternetStackHelper ipStack;
+        ipStack.Install (cabledNodes);
+
+
+         // Assing the ip addressed to each cabled node
+         NS_LOG_INFO ("Assign ip addresses.");
+         Ipv4AddressHelper ip;
+         ip.SetBase ("192.168.0.7", "255.255.255.0");
+         Ipv4InterfaceContainer addresses = ip.Assign (devices.Get (0));
+
+         Ipv4AddressHelper ip1;
+         ip1.SetBase ("192.168.0.8", "255.255.255.0");
+         Ipv4InterfaceContainer addresses1 = ip1.Assign (devices.Get (1));
+
+         Ipv4AddressHelper ip2;
+         ip2.SetBase ("192.168.0.9", "255.255.255.0");
+         Ipv4InterfaceContainer addresses2 = ip2.Assign (devices.Get (2));
+
+         Ipv4AddressHelper ip3;
+         ip3.SetBase ("192.168.0.10", "255.255.255.0");
+         Ipv4InterfaceContainer addresses3 = ip3.Assign (devices.Get (3));
+
+        Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
         Simulator::Run ();
         Simulator::Destroy ();
